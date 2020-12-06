01a175dfa23ae2a47f5624e9d2700910d23f05f9
Luis Carlos Arrieta Sierra <lcarrietas@unal.edu.co>
2020-12-05 23:47:08 -0500

    Integrantes hechos

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,14 +1,27 @@
 <template>
-    <div>
-
-    </div>
+    
+      <div class="card text-white bg-dark">
+        <div class="d-flex justify-content-center p-2">
+        <img :src="member.Imagen" alt="Foto del Integrante">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"> {{member.Nombre}}</h5>
+          <p class="card-text">   {{member.Descripcion}} </p>
+          
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Grupo 192</small>
+        </div>
+      </div>
+    
+  
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    }
+export default {
+  name: "TeamCard",
+  props: ["member"],
+};
 </script>
 
 <style scoped>
