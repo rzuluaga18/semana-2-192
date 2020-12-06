2b9f6e8be35c7e9d8c3c8b9a701a37e0db60332e
rzuluaga18 <jrzp18@gmail.com>
2020-12-05 21:55:49 -0500

    Seccion con API funcionando

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <template>
+
   <div id="app">
+    
+ 
+
+      <!-- INICIO SECCIÓN API -->
+        <div class="container-fluid mt-4">
+       
+           
+      <api-seccion>   </api-seccion>      
+       
+      </div>
+
+      <!-- FIN SECCIÓN API -->
+      
+   
+    
+       
+    
+
   </div>
+
 </template>
 
 <script>
+import ApiSeccion from './components/ApiSeccion.vue'
+
+
 
 export default {
   name: 'App',
+  
+  components: { 
+    ApiSeccion 
+    },
+  
+
+  data(){
+    return{
+      
+    }
+  }
 }
 </script>
 
