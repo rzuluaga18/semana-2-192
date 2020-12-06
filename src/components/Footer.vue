6b365a7aa6229e3002a14b4714a3e2dcbae5b74f
Luis Carlos Arrieta Sierra <lcarrietas@unal.edu.co>
2020-12-05 23:25:58 -0500

    footer completado

---
+++ src/components/Footer.vue
@@ -0,0 +1,77 @@
+<template>
+<!-- FOOTER  -->
+<div>
+  <footer class="page-footer bg-secondary text-white pt-4">
+    <!-- Footer Text -->
+    <div class="container-fluid text-center text-md-left">
+      <!-- Grid row -->
+      <div class="row">
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none pb-3" />
+        <!-- Grid column -->
+        <div class="col-md-6 mb-md-0 mb-3">
+          <!-- Content -->
+          <h5 class="text-uppercase font-weight-bold">Angie Sofía Benito González</h5>
+                <p>
+                
+            <strong>Rol:</strong> Frontend y Tester.
+            <br>
+            <strong>Contacto:</strong> angiesofia98@hotmail.com 
+              </p>
+        </div>
+                <div class="col-md-6 mb-md-0 mb-3">
+          <!-- Content -->
+          <h5 class="text-uppercase font-weight-bold">Luis Carlos Arrieta Sierra</h5>
+                <p>
+            
+              <strong>Rol:</strong> Analista de Software y Backend.
+              <br>
+              <strong>Contacto:</strong> lcarrietas@unal.edu.co 
+              </p>
+        </div>
+                <div class="col-md-6 mb-md-0 mb-3">
+          <!-- Content -->
+          <h5 class="text-uppercase font-weight-bold">Ramiro Andrés Zuluaga Parra</h5>
+                <p>
+                
+            <strong>Rol:</strong> Arquitecto de Software y Frontend.
+            <br>
+            <strong>Contacto:</strong> jrzp18@gmail.com
+              </p>
+        </div>
+                        <div class="col-md-6 mb-md-0 mb-3">
+          <!-- Content -->
+          <h5 class="text-uppercase font-weight-bold">Cristiam David Bustos Fuentes</h5>
+                <p>            
+                
+            <strong>Rol:</strong> Backend.
+            <br>
+            <strong>Contacto:</strong> cristiambus@gmail.com
+              </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+    <!-- Footer Text -->
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-1 bg-dark">
+                <h5 class="text-uppercase font-weight-bold">Reto de la semana 1 </h5>
+          <p>
+            
+          </p>
+          <p><a href="https://github.com/rzuluaga18/semana-1-192.git">link repositorio >></a></p>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  </div>
+  <!-- FIN FOOTER  -->
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
